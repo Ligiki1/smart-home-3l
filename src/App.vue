04b646d2325c7a9e89e4fd0de4dac24736670a5e
Ligiki1 <56873825+Ligiki1@users.noreply.github.com>
2022-01-02 14:12:40 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+   <webcam />
+   <span>Door Detection 3ML {{ msg }}</span>
+  </div>
+</template>
+
+<script>
+import webcam from './components/webcam.vue'
+
+export default {
+  name: 'App',
+  components: {
+    webcam
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/webcam.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+      <video ref="video">Stream unavailable</video>
+      </div>
+</template>
+
+<script>
+export default {
+    name: "webcam",
+    mounted() {
+        this.video = this.$refs.video
+        this.startCapture()
+    },
+    methods: {
+        startCapture() {
+            navigator.mediaDevices.getUserMedia({video: true, audio: false}).then(stream =>{
+                this.video.srcObject = stream
+                this.video.play()
+            }).catch(error => {
+                console.log(error)
+            })
+        }
+    },
+    data() {
+        return {
+            video:null
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>>
